6c76fe085dece7a14dd960ef619282f5bc356c5b
Sergio Ernesto Rosas Ducoing <rosasducoing@gmail.com>
2026-03-03 17:31:32 -0600

    Primer commit: Mi proyecto React

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+//const element = <h1>React is {5+5} times better with JSX</h1>
+/* const firstNumber = 5.5
+const secondNumber = 50
+const element = <h1>React is {firstNumber + secondNumber} times better with JSX</h1> */
+const isEnabled = true
+const placeholder = "other value"
+const size = 50
+
+const element = (
+  <>
+    <p>Un parrafo</p>
+    <p>Segundo parrafo</p>
+  </>
+
+)
+
+createRoot(document.getElementById('root')).render(
+  //  <StrictMode>
+  //  <App />
+  //</StrictMode>,
+
+  //<h1>Hello vite + React</h1>, // Con JSX
+
+  //React.createElement('h1', null, 'Hello vite + React') // Sin el JSX
+  element,
+
+  <input placeholder={placeholder} size={size} disabled={!isEnabled} />
+)
